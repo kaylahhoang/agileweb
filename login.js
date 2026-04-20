e31c3b4d5a5152038d5a753223d5b2e68f46eb2e
Kennice Leong <kenniceleong@gmail.com>
2026-04-20 23:15:45 +0800

    Cleaned up login UI, still needs more work, other pages incomplete

--- login.js
+++ login.js
@@ -1,29 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     const wrapper = document.getElementById('wrapper');
 
-    wrapper.style.transform = "translateX(0)";
 
-    const showRegister = document.getElementById('showRegister');
-    const showLogin = document.getElementById('showLogin');
-    
 
-    showRegister.addEventListener('click', () => {
+    document.getElementById('showRegister').addEventListener('click', () => {
+
         wrapper.style.transform = "translateX(-100vw)";
-
-        setTimeout(() => {
-        wrapper.style.transform = "translateX(-200vw)";
-    }, 400);
     });
 
-    
-
-    
-    showLogin.addEventListener('click', () => {
-        wrapper.style.transform = "translateX(-100vw)";
-
-        setTimeout(() => {
-            wrapper.style.transform = "translateX(0)";
-        }, 400);
+    document.getElementById('showLogin').addEventListener('click', () => {
+        wrapper.style.transform = "translateX(0)";
     });
-
 });
